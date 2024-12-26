6c9b2c8e3e96b86588caae958958618742b987d4
kho thach <khothach@gmail.com>
2024-12-26 18:13:15 +1300

    updated intro and thought bubble

--- src/data/aboutMe.js
+++ src/data/aboutMe.js
@@ -1,5 +1,5 @@
 export const aboutMe = [{
     aboutMe: "",
-    pageIntro: "Hello, My name is Kho and I am passionate about web developing. Whether it is Frontend, Backend or Fullstack developing. I started getting interested in coding in some way when I played games in my younger years and have decided to pursue a career in the IT world. I have many projects to demonstrate my ability to code and problem solve.",
-    thoughtBubble: "It is interesting how they created this game, maybe I should get into coding sometime in the future!"
+    pageIntro: "Hello, my name is Kho, and I am passionate about web development. Whether it's frontend, backend, or full-stack development, I love building solutions that make an impact. My interest in coding began during my younger years when I played video games, sparking a curiosity about how things work behind the scenes. This led me to pursue a career in the IT world. Over the years, I've worked on numerous projects that showcase my ability to code, problem-solve, and innovate.",
+    thoughtBubble: "It's fascinating to think about how this game was created—maybe it's time to start coding and explore this world further!"
 }]
